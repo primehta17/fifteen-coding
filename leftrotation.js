864cea2d3955e336ad484c99f50c78cf95cb4883
Priyanka Mehta <primehta17@gmail.com>
2026-01-08 18:53:24 +0530

    five

---
+++ leftrotation.js
@@ -0,0 +1,26 @@
+let arr = [2, 23, 44, 21, 46];
+// newarr=[23,44,21,46,2];
+// newarr=[44,21,46,2,23];
+// newarr=[21,46,2,23,44];
+function leftroatationone(arr) {
+  let first = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i + 1]; // newarr.push(arr[i + 1]);
+  }
+  arr[arr.length - 1] = first;
+  return arr;
+}
+console.log(leftroatationone(arr));
+
+function leftroatationkth(arr, k) {
+  for (let j = 0; j < k; j++) {
+    let first = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i + 1];
+    }
+    arr[arr.length - 1] = first;
+  }
+
+  return arr;
+}
+console.log(leftroatationkth(arr, 3));
