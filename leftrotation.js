e9fd25d52147386402be88311a2e4fe0d4b9758a
Priyanka Mehta <primehta17@gmail.com>
2026-01-08 19:29:26 +0530

    five

--- leftrotation.js
+++ leftrotation.js
@@ -1,7 +1,9 @@
 let arr = [2, 23, 44, 21, 46];
-// newarr=[23,44,21,46,2];
-// newarr=[44,21,46,2,23];
-// newarr=[21,46,2,23,44];
+let arr1 = [2, 23, 44, 21, 46];
+let arr2 = [2, 23, 44, 21, 46];
+// newarr=[23,44,21,46,2];//1
+// newarr=[44,21,46,2,23];//2
+// newarr=[21,46,2,23,44];//3
 function leftroatationone(arr) {
   let first = arr[0];
   for (let i = 0; i < arr.length - 1; i++) {
@@ -12,19 +14,34 @@ function leftroatationone(arr) {
 }
 console.log(leftroatationone(arr));
 
-function leftroatationkth(arr, k) {
-  // if (arr.length % k == 0) {
-  //   return arr;
-  // }
+function leftRotationKth(arr, k) {
   k = k % arr.length;
+
   for (let j = 0; j < k; j++) {
     let first = arr[0];
+
     for (let i = 0; i < arr.length - 1; i++) {
       arr[i] = arr[i + 1];
     }
+
     arr[arr.length - 1] = first;
   }
 
   return arr;
 }
-console.log(leftroatationkth(arr, 4));
+
+console.log(leftRotationKth(arr, 3));
+
+//optimize
+
+function optimizeLeftRotation(arr, k) {
+  let temp = [];
+  k = k % arr.length;
+
+  for (let i = 0; i < arr.length; i++) {
+    temp[i] = arr[(i + k) % arr.length];
+  }
+
+  return temp;
+}
+console.log(optimizeLeftRotation(arr2, 3));
